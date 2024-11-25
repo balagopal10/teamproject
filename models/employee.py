35e740eb3b9057197d159140c8241e554450a45f
Bala10 <balagopalms2002@gmail.com>
2024-11-25 17:29:46 +0530

    Class employee committed

--- models/employee.py
+++ models/employee.py
@@ -1,7 +1,9 @@
 class Employee:
-    def  __init__(self,employeeName,age):
+    def  __init__(self,employeeName,age,doj,createdOn,isActive):
         self.employeeName= employeeName
         self.age=age
+        self.doj=doj
+        self.createdOn=createdOn
+        self.isActive=isActive
 
-    def display(self):
-        print(f"The employee name is {self.employeeName} and age is {self.age}") 
+    
